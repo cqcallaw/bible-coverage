1985803c593a4438279e0fdab3b391f369578582
Caleb Callaway <caleb@brainvitamins.net>
2024-05-18 16:18:41 -0700

    Formatting

--- bibles/nasb/nasb1971/bible.py
+++ bibles/nasb/nasb1971/bible.py
@@ -3,10 +3,13 @@ import re
 from typing import List
 from model import Bible, Book, Chapter, Verse
 
-def parse(bible_text_path: str = os.path.join(os.path.dirname(__file__), 'bible.txt')) -> Bible:
+
+def parse(
+    bible_text_path: str = os.path.join(os.path.dirname(__file__), "bible.txt"),
+) -> Bible:
     books: Bible = []
     with open(bible_text_path) as file:
-        current_book_name: str = 'Genesis'
+        current_book_name: str = "Genesis"
         current_book_list: List[Book] = []
         current_chapter_list: List[Chapter] = []
         current_chapter_number: int = 1
@@ -27,7 +30,9 @@ def parse(bible_text_path: str = os.path.join(os.path.dirname(__file__), 'bible.
 
                 if current_chapter_number != matched_chapter_number:
                     # chapter boundary
-                    current_chapter_list.append(Chapter(current_chapter_number, current_chapter_verses))
+                    current_chapter_list.append(
+                        Chapter(current_chapter_number, current_chapter_verses)
+                    )
                     current_chapter_verses = []
                     current_chapter_number = matched_chapter_number
                 if current_book_name != matched_book_name:
@@ -40,4 +45,4 @@ def parse(bible_text_path: str = os.path.join(os.path.dirname(__file__), 'bible.
     current_book_list.append(Chapter(current_chapter_number, current_chapter_verses))
     books.append(Book(current_book_name, current_chapter_list))
 
-    return Bible('NASB 1971', books)
+    return Bible("NASB 1971", books)
