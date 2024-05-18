a61133d31b01d64e05a7443ec63ee4c6a31fb707
Caleb Callaway <caleb@brainvitamins.net>
2024-05-18 16:23:32 -0700

    Move to src folder

--- src/bible-coverage.py
+++ src/bible-coverage.py
@@ -5,5 +5,4 @@ bible = bible.parse()
 for book in bible.books:
     for chapter in book.chapters:
         for verse in chapter.verses:
-            print (f"{book.name} {chapter.number}:{verse.number}")
-
+            print(f"{book.name} {chapter.number}:{verse.number}")
